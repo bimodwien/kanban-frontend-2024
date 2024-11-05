93a3abc80b8c5c7aadab9aaa6157519b0bc3d997
bimodwien <bimodwien2@gmail.com>
2024-11-05 11:06:07 +0700

    update: axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,9 +5,6 @@ export function axiosInstance(): AxiosInstance {
   const token = getCookie("access_token") || "";
   return axios.create({
     baseURL: "https://kanban-backend-2024-production.up.railway.app/api/",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
     withCredentials: true,
   });
 }
