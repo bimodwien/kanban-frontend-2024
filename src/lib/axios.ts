d2c1786b3f1480297fa4274d57ae87cc6aea91da
bimodwien <bimodwien2@gmail.com>
2024-12-16 13:49:04 +0700

    update: token di axios tidak jadi dipakai

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,6 @@ import axios, { AxiosInstance } from "axios";
 import { getCookie } from "cookies-next";
 
 export function axiosInstance(): AxiosInstance {
-  const token = getCookie("access_token") || "";
   return axios.create({
     baseURL: "https://kanban-backend-2024-production.up.railway.app/api/",
     withCredentials: true,
