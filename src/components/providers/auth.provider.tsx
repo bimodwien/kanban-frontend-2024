20a90a3eb91017c8a401029970b01c64362b4943
bimodwien <bimodwien2@gmail.com>
2024-12-16 13:47:11 +0700

    update: debugging on mobile

--- src/components/providers/auth.provider.tsx
+++ src/components/providers/auth.provider.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useAppDispatch } from "@/lib/redux/hooks";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
 import { keepLogin } from "@/lib/redux/middleware/auth.middleware";
 
 type Props = { children: React.ReactNode };
 
 const AuthProvider = ({ children }: Props) => {
   const dispatch = useAppDispatch();
+  const [isReady, setIsReady] = useState(false);
+  const auth = useAppSelector((state) => state.auth);
 
   useEffect(() => {
-    dispatch(keepLogin());
-  }, [dispatch]);
+    if (auth.isLoaded) {
+      dispatch(keepLogin()).finally(() => setIsReady(true));
+    } else {
+      setIsReady(true);
+    }
+  }, [dispatch, auth.isLoaded]);
+
+  if (!isReady) {
+    return null; // or a loading spinner
+  }
 
   return <>{children}</>;
 };
